9fe29283a0f62c613d40f42686e01ab595c400af
Alunno <Alunno@PC-26-INF4>
2017-10-12 09:54:20 +0200

    Primo commit

---
+++ ProvaFabio/src/Uno.java
@@ -0,0 +1,13 @@
+
+public class Uno {
+
+	public Uno() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}

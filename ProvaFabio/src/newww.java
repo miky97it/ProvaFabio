75fe80936f74a288c0eb0c4180f1dba2f4f50f4f
Alexandre Pugnaghi <pugnaghi8@gmail.com>
2017-10-17 11:58:55 +0200

    ECCO

--- ProvaFabio/src/newww.java
+++ ProvaFabio/src/newww.java
@@ -7,7 +7,7 @@ public class newww {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ASENSIO 4-1 è FINITA!");
+		System.out.println("ASENSIO 4-1 è FINITA!!");
 	}
 
 }
